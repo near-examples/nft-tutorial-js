b70788ad9c0fedc2067f4c1776094145ce83427c
BenKurrek <bkurrek@uwaterloo.ca>
2022-08-06 21:32:40 -0400

    made all fns take object args

--- src/nft-contract/approval.ts
+++ src/nft-contract/approval.ts
@@ -7,45 +7,62 @@ import { Token } from "./metadata";
 const GAS_FOR_NFT_ON_APPROVE = 35_000_000_000_000;
 
 //approve an account ID to transfer a token on your behalf
-export function internalNftApprove(
+export function internalNftApprove({
+    contract,
+    tokenId,
+    accountId,
+    msg
+}:{ 
     contract: Contract, 
     tokenId: string, 
     accountId: string, 
-    msg: string
-) {
+    msg: string 
+}) {
     /*
         FILL THIS IN
     */
 }
 
 //check if the passed in account has access to approve the token ID
-export function internalNftIsApproved(
+export function internalNftIsApproved({
+    contract,
+    tokenId,
+    approvedAccountId,
+    approvalId
+}:{ 
     contract: Contract, 
-    tokenId: string, 
+    tokenId: string,
     approvedAccountId: string, 
-    approvalId: number
-) {
+    approvalId: number 
+}) {
     /*
         FILL THIS IN
     */
 }
 
 //revoke a specific account from transferring the token on your behalf
-export function internalNftRevoke(
+export function internalNftRevoke({
+    contract,
+    tokenId,
+    accountId
+}:{ 
     contract: Contract, 
     tokenId: string, 
-    accountId: string
-) {
+    accountId: string 
+}) {
     /*
         FILL THIS IN
     */
 }
 
 //revoke all accounts from transferring the token on your behalf
-export function internalNftRevokeAll(
+export function internalNftRevokeAll({
+    contract,
+    tokenId
+}:{ 
     contract: Contract, 
-    tokenId: string
-) {
+    tokenId: string 
+}) {
     /*
         FILL THIS IN
     */
